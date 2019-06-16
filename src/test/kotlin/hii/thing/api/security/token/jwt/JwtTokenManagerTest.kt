121799b6f391d3e41c3e90f08b9dac206915f5c8
Thanachai Thongkum X1 Carbon2017 <singmax161@gmail.com>
2019-06-16 12:01:19 +0700

    update security jwt test.

---
+++ src/test/kotlin/hii/thing/api/security/token/jwt/JwtTokenManagerTest.kt
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2019 NSTDA
+ *   National Science and Technology Development Agency, Thailand
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package hii.thing.api.security.token.jwt
+
+import com.auth0.jwt.JWT
+import com.auth0.jwt.algorithms.Algorithm
+import hii.thing.api.JwtConst
+import org.amshove.kluent.`should be equal to`
+import org.amshove.kluent.`should equal`
+import org.junit.Test
+import java.security.interfaces.RSAPrivateKey
+import java.security.interfaces.RSAPublicKey
+import java.util.Date
+import java.util.UUID
+
+class JwtTokenManagerTest {
+
+    val jwtTokenManager = JwtTokenManager()
+    val roles = listOf("MACHINE", "abc")
+    val scope = listOf("/regis", "/auth")
+    val deviceName = "hii/007"
+
+    @Test
+    fun verify() {
+        val accessToken = createAccessToken()
+        jwtTokenManager.verify(accessToken) `should be equal to` true
+    }
+
+    @Test
+    fun getUserRole() {
+        val accessToken = createAccessToken()
+        jwtTokenManager.getUserRole(accessToken) `should equal` roles
+    }
+
+    @Test
+    fun getName() {
+        val accessToken = createAccessToken()
+        jwtTokenManager.getName(accessToken) `should be equal to` deviceName
+    }
+
+    private fun createAccessToken(expire: Long = 1000000, issuer: String = JwtConst.issuer): String {
+        val publicKey: RSAPublicKey = JwtConst.keyPair.public as RSAPublicKey
+        val privateKey: RSAPrivateKey = JwtConst.keyPair.private as RSAPrivateKey
+        val algorithm = Algorithm.RSA512(publicKey, privateKey)
+        val date = Date()
+
+        return JWT.create()
+            .withIssuer(issuer)
+            .withIssuedAt(date)
+            .withExpiresAt(Date(date.time + expire))
+            .withJWTId(UUID.randomUUID().toString())
+            .withNotBefore(date)
+            .withSubject(deviceName)
+            .withArrayClaim("role", roles.toTypedArray())
+            .withArrayClaim("scopt", scope.toTypedArray())
+            .sign(algorithm)
+    }
+}
