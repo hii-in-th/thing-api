8682af1fd9cea5dd9fa1852d4b5107d838942b58
Thanachai Thongkum X1 Carbon2017 <singmax161@gmail.com>
2019-08-28 13:17:17 +0700

    add HistoryWeightDao

---
+++ src/main/kotlin/hii/thing/api/dao/history/weight/HistoryWeightDao.kt
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2019 NSTDA
+ *   National Science and Technology Development Agency, Thailand
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package hii.thing.api.dao.history.weight
+
+import hii.thing.api.dao.Dao
+import hii.thing.api.vital.Weight
+
+interface HistoryWeightDao : Dao {
+    fun save(citizenId: String, weight: Weight): List<Weight>
+    fun get(citizenId: String): List<Weight>
+}
